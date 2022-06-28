1ea824c32580ca5834950363280efeb43418e600
wadebooth <wademgmt@gmail.com>
2022-06-27 21:57:12 -0400

    added pause and play changes

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styles from '../styles/AudioPlayer.module.css'
 import {
   AiOutlineArrowLeft,
   AiOutlineArrowRight,
-  AiOutlinePlayCircle,
-  AiOutlinePauseCircle,
+  AiOutlinePause,
 } from 'react-icons/ai'
+import { BiPlay } from 'react-icons/bi'
 
 const AudioPlayer = () => {
-  //sets the state of whether the song is playing or not.
+  //STATE
   const [isPlaying, setIsPlaying] = useState(false)
+
+  //REFERENCES
+  const audioPlayer = useRef() //reference our audio component
 
   //when the play/pause button is clicked, it'll run the function which toggles the state of isPlaying.
   const togglePlayPause = () => {
-    setIsPlaying(!isPlaying)
+    const prevValue = isPlaying
+    setIsPlaying(!prevValue)
+    if (!prevValue) {
+      audioPlayer.current.play()
+    } else {
+      audioPlayer.current.pause()
+    }
   }
 
   return (
     <div className={styles.AudioPlayer}>
-      <audio src=''></audio>
+      <audio
+        ref={audioPlayer}
+        src='https://artlist.io/song/15034/thinking-of-you.mp3'
+      ></audio>
       <button className={styles.forwardBackward}>
         <AiOutlineArrowLeft /> 15
       </button>
       <button onClick={togglePlayPause} className={styles.playPause}>
-        {isPlaying ? (
-          <AiOutlinePauseCircle />
-        ) : (
-          <AiOutlinePlayCircle className={styles.play} />
-        )}
+        {isPlaying ? <AiOutlinePause /> : <BiPlay className={styles.play} />}
       </button>
       <button className={styles.forwardBackward}>
         <AiOutlineArrowRight /> 15
